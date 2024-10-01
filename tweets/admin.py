54eb15e9c7d53b045fad1c2e8cefcdd328ac6863
Je Seung You (Jason) <jy2908@columbia.edu>
2024-10-01 16:33:08 -0400

    tutorial -3

--- tweets/admin.py
+++ tweets/admin.py
@@ -13,9 +13,29 @@ class UserAdmin(admin.ModelAdmin):
     pass
 
 
+class ElonMuskFilter(admin.SimpleListFilter):
+    title = "Elon Musk"
+    parameter_name = "elon_musk"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("contains", "Contains Elon Musk"),
+            ("not_contains", "Does Not Contain Elon Musk"),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == "contains":
+            return queryset.filter(payload__icontains="elon musk")
+        if self.value() == "not_contains":
+            return queryset.exclude(payload__icontains="elon musk")
+        return queryset
+
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = ("payload", "user", "created_at", "updated_at", "like_count")
+    search_fields = ("payload", "user__name")
+    list_filter = ("created_at", ElonMuskFilter)
 
     def like_count(self, obj):
         return obj.likes.count()
@@ -25,4 +45,5 @@ class TweetAdmin(admin.ModelAdmin):
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ("user__name",)
+    list_filter = ("created_at",)
